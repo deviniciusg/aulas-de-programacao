726cbddb9fbf14af87808e7d564dcfaad540b99c
deviniciusg <vinicius.abril12@gmail.com>
2024-04-16 21:06:13 -0300

    Estudos teoricos de JS

---
+++ meu_site_institucional/script.js
@@ -0,0 +1,29 @@
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.btn')
+const curtoBt = document.querySelector('.btn')
+const banner = document.querySelector('.app__image')
+
+focoBt.addEventListener('click', () =>
+    html.setAttribute('data-contexto', 'foco')
+)
+
+focoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', '/imagens/foco.png')
+})
+
+// aula 02 atividade 04 criando funções
+
+focoBt.addEventListener('click', () => {
+    alterarContexto('foco')
+})
+
+
+function alterarContexto() {
+    html.setAttribute('data-contexto', contexto)
+}
+
+function alterarContexto() {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
