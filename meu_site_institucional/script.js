4cdd6685ddd48405a3d229150c51bb00d84b2e44
deviniciusg <vinicius.abril12@gmail.com>
2024-04-18 20:48:21 -0300

    Adicionando responsabilidade

--- meu_site_institucional/script.js
+++ meu_site_institucional/script.js
@@ -1,39 +1,6 @@
 const html = document.querySelector('html')
-const focoBt = document.querySelector('.btn')
-const curtoBt = document.querySelector('.btn')
-const banner = document.querySelector('.app__image')
+const menu = document.querySelector('menu')
 
-focoBt.addEventListener('click', () =>
+menu.addEventListener('click', () =>
     html.setAttribute('data-contexto', 'foco')
 )
-
-focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', '/imagens/foco.png')
-})
-
-// aula 02 atividade 04 criando funções
-
-focoBt.addEventListener('click', () => {
-    alterarContexto('foco')
-})
-
-
-function alterarContexto() {
-    html.setAttribute('data-contexto', contexto)
-}
-
-function alterarContexto() {
-    html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src', `/imagens/${contexto}.png`)
-}
-
-// 02 05 alterando texto da página
-
-<h1 class="app__title">
-    Otimize sua produtividade,<br>
-    <strong class="app__title-strong">mergulhe no que importa.</strong>
-</h1>
-
-
-const titulo = document.querySelector('.app__title')
