5729c99758ed90fcf76572b98f873bb355b2416a
deviniciusg <vinicius.abril12@gmail.com>
2024-04-19 22:01:20 -0300

    Ajustes responsabilidade

--- meu_site_institucional/script.js
+++ meu_site_institucional/script.js
@@ -1,6 +1,14 @@
+console.log('olá mundo');
 const html = document.querySelector('html')
-const menu = document.querySelector('menu')
+const menu_mobile = document.getElementById('menu_mobile')
 
-menu.addEventListener('click', () =>
-    html.setAttribute('data-contexto', 'foco')
-)
+
+function minhaFuncao(){
+    if (menu_mobile.style.display == 'none') {
+    console.log('Botão apertado! Display none.')
+    menu_mobile.style.display = 'flex';
+    } else {
+    console.log('Botão apertado com sucesso! Display flex.')
+    menu_mobile.style.display = 'none';    
+    }
+}
