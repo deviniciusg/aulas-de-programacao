30a6dafd9330bc1536329d65cc5f47936b77b8c2
deviniciusg <vinicius.abril12@gmail.com>
2024-04-19 22:05:23 -0300

    Ajustes na função

--- meu_site_institucional/script.js
+++ meu_site_institucional/script.js
@@ -1,9 +1,11 @@
 console.log('olá mundo');
 const html = document.querySelector('html')
 const menu_mobile = document.getElementById('menu_mobile')
+menu_mobile.style.display = 'none';    
 
 
-function minhaFuncao(){
+
+function minhaFuncao(){    
     if (menu_mobile.style.display == 'none') {
     console.log('Botão apertado! Display none.')
     menu_mobile.style.display = 'flex';
